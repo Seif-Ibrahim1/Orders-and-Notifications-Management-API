9f19b25a134bb83fe35a542a77654eb61a7860e9
Seif <SE.SeifIbrahim@gmail.com>
2023-12-29 09:58:17 +0200

    Added the base project files

---
+++ orders-management/src/main/java/com/example/ordersmanagement/OrdersManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.ordersmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersManagementApplication.class, args);
+	}
+
+}
+
+
---
+++ orders-management/src/test/java/com/example/ordersmanagement/OrdersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordersmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
